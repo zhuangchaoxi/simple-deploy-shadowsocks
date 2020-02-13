4ede79276e72f364ed727c8bdeeabb0345a56798
zhuangchaoxi <550358270@qq.com>
2020-02-14 01:25:33 +0800

    Update createShadowsockServer.sh

--- createShadowsockServer.sh
+++ createShadowsockServer.sh
@@ -18,9 +18,9 @@ sed -i "s/\"password\":\".*\"/\"password\":\"${password}\"/g" ${shadow_cfg}
 firewall-cmd --zone=public --add-port=${port}/tcp --permanent
 firewall-cmd --reload
 # change ssh config
-echo ClientAliveInterval 60 >> ${ssh_cfg} ＃server每隔60秒发送一次请求给client，然后client响应，从而保持连接
-echo ClientAliveCountMax 1440 >> ${ssh_cfg} ＃server发出请求后，客户端没有响应得次数达到3，就自动断开连接，正常情况下，client不会不响应
-systemctl restart sshd
+#echo ClientAliveInterval 60 >> ${ssh_cfg} ＃server每隔60秒发送一次请求给client，然后client响应，从而保持连接
+#echo ClientAliveCountMax 3 >> ${ssh_cfg} ＃server发出请求后，客户端没有响应得次数达到3，就自动断开连接
+#systemctl restart sshd
 # install bbr
 wget –no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 chmod +x *.sh
