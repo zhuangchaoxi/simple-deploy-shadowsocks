e1d701b55bb5d10882cadd00751cb8173edc65b2
zhuangchaoxi <550358270@qq.com>
2020-02-13 23:30:27 +0800

    Update change_port.sh

--- change_port.sh
+++ change_port.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+shadow_cfg=/etc/shadowsocks.json
 port=$1
 ip=$2
-sed -i "s/\"server_port\":.*,/\"server_port\":${port},/g" /etc/shadowsocks.json
+sed -i "s/\"server_port\":.*,/\"server_port\":${port},/g" ${shadow_cfg}
 if [ -n "${ip}" ]; then
-   
+   sed -i "s/\"server\":\".*\"/\"server\":\"${ip}\"/g" ${shadow_cfg}
 fi
 sh restart.sh
 firewall-cmd --zone=public --add-port=${port}/tcp --permanent
